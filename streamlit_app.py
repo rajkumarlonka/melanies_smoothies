3ae05f4a2e2ec824b1563b280e92e5cb8581bafc
RAJKUMAR LONKA <67069619+rajkumarlonka@users.noreply.github.com>
2024-07-21 16:34:20 +0530

    Create streamlit_app.py
    connection changes needed bc to move from sis to Snis

---
+++ streamlit_app.py
@@ -0,0 +1,64 @@
+# Import python packages
+import streamlit as st
+from snowflake.snowpark.functions import col
+
+# Write directly to the app
+st.title("Customize Your Smoothie! :cup_with_straw:")
+st.write(
+    """Choose the fruits you want in your custom smoothie
+    """
+)
+
+cnx= st.connection('snowflake')
+session=cnx.session()
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+#st.dataframe(data=my_dataframe, use_container_width=True)
+
+
+ingredients_list = st.multiselect(
+    'Choose upto 5 ingredients:', my_dataframe
+)
+
+if ingredients_list:
+    #st.write(ingredients_list)
+    #st.text(ingredients_list)
+
+    ingredients_string=''
+    for fruit_chosen in ingredients_list:
+        ingredients_string+=fruit_chosen+' '
+    st.write(ingredients_string)
+    my_insert_stmt = """ insert into smoothies.public.orders(ingredients)
+            values ('""" + ingredients_string + """')"""
+
+    time_to_insert = st.button('Submit Order')
+    #st.write(my_insert_stmt)
+
+    if time_to_insert:
+        session.sql(my_insert_stmt).collect()
+        st.success('Your Smoothie is ordered!', icon="✅")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
